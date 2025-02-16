2a0d6e6a15a8fc778b31e6cb1512ac51c52f7cf0
Gregory Chernyavskiy <gregory.chernyavskiy@gmail.com>
2025-02-15 19:27:11 -0600

    .

--- gregory_chernyavskiy.assignment-2/main.c
+++ gregory_chernyavskiy.assignment-2/main.c
@@ -1,19 +1,39 @@
 #include "dungeon_generation.h"
+#include <stdio.h>
+#include <string.h>
 
-int main(void) {
+int main(int argc, char *argv[]) {
     srand(time(NULL));
+    int load = 0, save = 0; // Flags for --load and --save
 
-    emptyDungeon();
+    // Check command-line arguments
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--save") == 0) {
+            save = 1;
+        } else if (strcmp(argv[i], "--load") == 0) {
+            load = 1;
+        }
+    }
 
-    int countRooms = createRooms();
-    connectRooms(countRooms);
-    placeStairs(countRooms);
-    placePlayer(countRooms);
+    if (load) {
+        // If --load is provided, load the dungeon from file
+        loadDungeon();
+    } else {
+        // If no --load, generate a new dungeon
+        emptyDungeon();
+        int countRooms = createRooms();
+        connectRooms(countRooms);
+        placeStairs(countRooms);
+        placePlayer(countRooms);
+    }
 
+    // Print the dungeon after loading or generating
     printDungeon();
 
-    saveDungeon();
-    loadDungeon();
+    if (save) {
+        // If --save is provided, save the dungeon to file
+        saveDungeon();
+    }
 
     return 0;
 }
